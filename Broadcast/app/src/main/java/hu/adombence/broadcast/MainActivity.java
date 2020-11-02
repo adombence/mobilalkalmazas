7534d6495956c2f44d1eff319c7438b93ef4ca80
adomb <adom.bence97@gmail.com>
2020-11-02 16:31:01 +0100

    vegleges

--- Broadcast/app/src/main/java/hu/adombence/broadcast/MainActivity.java
+++ Broadcast/app/src/main/java/hu/adombence/broadcast/MainActivity.java
@@ -2,7 +2,10 @@ package hu.adombence.broadcast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.AudioDeviceInfo;
 import android.media.AudioManager;
 import android.os.Bundle;
@@ -33,6 +36,40 @@ public class MainActivity extends AppCompatActivity {
                 kep.setImageResource(R.drawable.headset);
                 allapot.setText(R.string.bedugva);
             }
+        }
+
+        IntentFilter szuro = new IntentFilter(Intent.ACTION_HEADSET_PLUG);
+
+        SajatFulesReceiver rec = new SajatFulesReceiver();
+        this.registerReceiver(rec, szuro);
+
+    }
+
+    public class SajatFulesReceiver extends BroadcastReceiver {
+
+        public int szamoljuk = 0;
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            AudioDeviceInfo[] eszkozok = manager.getDevices(AudioManager.GET_DEVICES_OUTPUTS);
+
+            boolean bentvan = false;
+
+            for (AudioDeviceInfo aud : eszkozok) {
+                if (aud.getType() == AudioDeviceInfo.TYPE_WIRED_HEADPHONES || aud.getType() == AudioDeviceInfo.TYPE_WIRED_HEADSET) {
+                    bentvan = true;
+                }
+            }
+            if (bentvan) {
+                allapot.setText(R.string.bedugva);
+                kep.setImageResource(R.drawable.headset);
+            } else {
+                allapot.setText(R.string.nincsbedugva);
+                kep.setImageResource(R.drawable.ic_baseline_help_24);
+            }
+            szamoljuk++;
+            szam.setText(getString(R.string.tv_szamol) + " " + szamoljuk);
+
         }
     }
 
