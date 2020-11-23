f8869653fa9d7a11aaed86b3f15249430e206ba5
adomb <adom.bence97@gmail.com>
2020-11-23 16:37:27 +0100

    Create lekerdezo activity

---
+++ sajatdb/app/src/main/java/hu/adombence/sajatdb/LekerdezoActivity.java
@@ -0,0 +1,14 @@
+package hu.adombence.sajatdb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LekerdezoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lekerdezo);
+    }
+}
