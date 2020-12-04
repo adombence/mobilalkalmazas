9eb9f941012cea203a64e08512751040b85737be
adomb <adom.bence97@gmail.com>
2020-12-04 21:35:47 +0100

    Update MainActivity.java

--- bmi_kalkultor/app/src/main/java/hu/adombence/bmi_kalkultor/MainActivity.java
+++ bmi_kalkultor/app/src/main/java/hu/adombence/bmi_kalkultor/MainActivity.java
@@ -4,7 +4,9 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,8 +16,13 @@ public class MainActivity extends AppCompatActivity {
     EditText testmagassag;
     Button szamol;
     TextView tv_eredmeny;
+    TextView tv_kivanatos;
+    TextView tv_szelsoertekek;
+    RadioButton ferfi;
+    RadioButton no;
+    boolean nem;
 
-    @SuppressLint("DefaultLocale")
+    @SuppressLint({"DefaultLocale", "ShowToast"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +32,26 @@ public class MainActivity extends AppCompatActivity {
         testmagassag = this.findViewById(R.id.testmagassag);
         szamol = this.findViewById(R.id.button);
         tv_eredmeny = this.findViewById(R.id.tv_eredmeny);
+        tv_kivanatos = this.findViewById(R.id.tv_kivanatos);
+        tv_szelsoertekek = this.findViewById(R.id.tv_szelsoertekek);
+        ferfi = this.findViewById(R.id.ferfi);
+        no = this.findViewById(R.id.no);
 
         szamol.setOnClickListener(view -> {
+            if (ferfi.isChecked()) {
+                nem = false;
+            } else if (no.isChecked()) {
+                nem = true;
+            }
+
+            Toast toast;
+            if (nem) {
+                toast = Toast.makeText(getApplicationContext(), R.string.no, Toast.LENGTH_SHORT);
+            } else {
+                toast = Toast.makeText(getApplicationContext(), R.string.ferfi, Toast.LENGTH_SHORT);
+            }
+            toast.show();
+
             double tomeg = Double.parseDouble(testtomeg.getText().toString());
             double magassag = Double.parseDouble(testmagassag.getText().toString());
             double eredmeny = tomeg / ((magassag / 100) * (magassag / 100));
@@ -41,7 +66,26 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 vissza = String.format("%.1f", eredmeny) + getString(R.string.elhizas);
             }
+
+            double kivanatosAtlag, min, max;
+            String kivanatos, szelsoErtekek;
+            if (nem) {
+                kivanatosAtlag = 20.8 * ((magassag / 100) * (magassag / 100));
+                min = 18.7 * ((magassag / 100) * (magassag / 100));
+                max = 23.8 * ((magassag / 100) * (magassag / 100));
+            } else {
+                kivanatosAtlag = 22 * ((magassag / 100) * (magassag / 100));
+                min = 20.1 * ((magassag / 100) * (magassag / 100));
+                max = 25 * ((magassag / 100) * (magassag / 100));
+            }
+
+            kivanatos = getString(R.string.kivanatos) + " " + String.format("%.1f", kivanatosAtlag) + getString(R.string.mertekegyseg_kg);
+            szelsoErtekek = getString(R.string.szelsoertek) + " " + String.format("%.1f", min) + getString(R.string.mertekegyseg_kg) + "-" + String.format("%.1f", max) + getString(R.string.mertekegyseg_kg);
+
             tv_eredmeny.setText(vissza);
+            tv_kivanatos.setText(kivanatos);
+            tv_szelsoertekek.setText(szelsoErtekek);
+
         });
     }
 }
