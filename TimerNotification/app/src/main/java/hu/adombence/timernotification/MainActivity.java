dd190bf77a4602799fdda2c0e6f350ebe5420cf3
adomb <adom.bence97@gmail.com>
2020-12-05 15:57:54 +0100

    create project

---
+++ TimerNotification/app/src/main/java/hu/adombence/timernotification/MainActivity.java
@@ -0,0 +1,14 @@
+package hu.adombence.timernotification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
