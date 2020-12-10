ac0ffcbbab4d6d84f9d8132fd8aa1cf0f6fa7a97
adomb <adom.bence97@gmail.com>
2020-12-10 19:20:17 +0100

    Update MainActivity.java

--- register/app/src/main/java/hu/adombence/register/MainActivity.java
+++ register/app/src/main/java/hu/adombence/register/MainActivity.java
@@ -3,12 +3,20 @@ package hu.adombence.register;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+     Button belepes;
+     Button regisztracio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        belepes = this.findViewById(R.id.belepes);
+        regisztracio = this.findViewById(R.id.regisztracio);
     }
 }
