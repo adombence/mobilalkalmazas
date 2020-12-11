57daeb527c9ede75ce7a99dcc4a0118aa78b12fb
adomb <adom.bence97@gmail.com>
2020-12-11 14:50:47 +0100

    Create activity_profile.java

---
+++ cukorbeteg/app/src/main/java/hu/adombence/cukorbeteg/activity_profile.java
@@ -0,0 +1,42 @@
+package hu.adombence.cukorbeteg;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_profile extends AppCompatActivity {
+
+    TextView textViewId, textViewUsername, textViewEmail, textViewGender;
+    Button buttonFooldal;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        init();
+
+    }
+
+    void init() {
+        textViewId = findViewById(R.id.textViewId);
+        textViewUsername = findViewById(R.id.textViewUsername);
+        textViewEmail = findViewById(R.id.textViewEmail);
+        buttonFooldal = this.findViewById(R.id.fooldal);
+        //getting the current user
+        User user = PrefManager.getInstance(this).getUser();
+
+        //setting the values to the textviews
+        textViewId.setText(String.valueOf(user.getId()));
+        textViewUsername.setText(user.getUsername());
+        textViewEmail.setText(user.getEmail());
+
+        //when the user presses logout button calling the logout method
+        findViewById(R.id.buttonLogout).setOnClickListener(view -> {
+            finish();
+            PrefManager.getInstance(getApplicationContext()).logout();
+        });
+
+    }
+}
