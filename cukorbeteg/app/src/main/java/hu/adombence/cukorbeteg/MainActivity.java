5c11aa14ea2bcfc3f8d21760d5673855c37f1067
adomb <adom.bence97@gmail.com>
2020-12-11 14:50:51 +0100

    Update MainActivity.java

--- cukorbeteg/app/src/main/java/hu/adombence/cukorbeteg/MainActivity.java
+++ cukorbeteg/app/src/main/java/hu/adombence/cukorbeteg/MainActivity.java
@@ -1,30 +1,38 @@
 package hu.adombence.cukorbeteg;
 
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.LinearLayout;
-
 public class MainActivity extends AppCompatActivity {
-
-    LinearLayout etkezesek;
-    Button[] buttons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
 
-        buttons = new Button[5];
-        etkezesek = findViewById(R.id.etkezesek);
-
-        /*for (int i = 0; i < 5; i++) {
-            buttons[i] = new Button(this);
-            buttons[i].setText("Button" + i);
-            etkezesek.addView(buttons[i]);
-
-        }*/
-
+    void init() {
+        Button profile = findViewById(R.id.btnProfile);
+        Button login = findViewById(R.id.btn_login);
+        Button sign_up = findViewById(R.id.btn_sign_up);
+        Button foodlal = findViewById(R.id.fooldal);
+        PrefManager prefManager = PrefManager.getInstance(MainActivity.this);
+        if (prefManager.isLoggedIn()) {
+            login.setVisibility(View.GONE);
+            sign_up.setVisibility(View.GONE);
+            profile.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, activity_profile.class)));
+            foodlal.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, MainActivity1.class)));
+        } else {
+            profile.setVisibility(View.GONE);
+            foodlal.setVisibility(View.GONE);
+            login.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, activity_login.class)));
+            sign_up.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, activity_signup.class)));
+        }
     }
 }
