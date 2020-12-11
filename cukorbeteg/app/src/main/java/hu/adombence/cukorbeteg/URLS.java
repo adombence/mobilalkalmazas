abdb56455d506a1d255e0fe63f9223baac00f0fc
adomb <adom.bence97@gmail.com>
2020-12-11 14:51:00 +0100

    Create URLS.java

---
+++ cukorbeteg/app/src/main/java/hu/adombence/cukorbeteg/URLS.java
@@ -0,0 +1,7 @@
+package hu.adombence.cukorbeteg;
+
+public class URLS {
+    private static final String URL_ROOT = "https://www.adombence.hu/serverside/api.php?action=";
+    public static final String URL_REGISTER = URL_ROOT + "signup";
+    public static final String URL_LOGIN = URL_ROOT + "login";
+}
