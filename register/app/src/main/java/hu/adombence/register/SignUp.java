3c57d2b9e9941076d2c77d21d8112f6c55c1c111
adomb <adom.bence97@gmail.com>
2020-12-11 11:13:26 +0100

    Update SignUp.java

--- register/app/src/main/java/hu/adombence/register/SignUp.java
+++ register/app/src/main/java/hu/adombence/register/SignUp.java
@@ -1,37 +1,49 @@
 package hu.adombence.register;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.content.Intent;
-import android.opengl.ETC1;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class SignUp extends AppCompatActivity {
 
-    private Button regisztracio;
     private EditText regName;
     private EditText regPass;
 
     public static Credentials credentials;
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor sharedPreferencesEditor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        regisztracio = this.findViewById(R.id.reg);
+        Button regisztracio = this.findViewById(R.id.reg);
         regName = this.findViewById(R.id.etRegEmail);
         regPass = this.findViewById(R.id.etRegPassword);
+
+        sharedPreferences = getApplicationContext().getSharedPreferences("CredentialsDB", MODE_PRIVATE);
+        sharedPreferencesEditor = sharedPreferences.edit();
+
 
         regisztracio.setOnClickListener(view -> {
             String regUsername = regName.getText().toString();
             String regPassword = regPass.getText().toString();
 
-            if(validate(regUsername, regPassword)){
+            if (validate(regUsername, regPassword)) {
                 credentials = new Credentials(regUsername, regPassword);
+
+                /* Store the Credentials */
+                sharedPreferencesEditor.putString("UserName", regUsername);
+                sharedPreferencesEditor.putString("Password", regPassword);
+                sharedPreferencesEditor.apply();
+
                 startActivity(new Intent(SignUp.this, MainActivity.class));
                 Toast.makeText(SignUp.this, "regisztráció sikeres!", Toast.LENGTH_SHORT).show();
             }
