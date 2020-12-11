57268d12caaca8626eae72987cb9ba5962c4bcce
adomb <adom.bence97@gmail.com>
2020-12-11 18:39:20 +0100

    Create datas.java

---
+++ cukorbeteg/app/src/main/java/hu/adombence/cukorbeteg/datas.java
@@ -0,0 +1,7 @@
+package hu.adombence.cukorbeteg;
+
+public class datas {
+    private static final String URL_ROOT = "https://www.adombence.hu/serverside/datas.php?action=";
+    public static final String URL_POST = URL_ROOT + "POST";
+    public static final String URL_GET = URL_ROOT + "GET";
+}
