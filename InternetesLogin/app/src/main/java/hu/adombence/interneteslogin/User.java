a59cced8e836e3ca4ebd0fe9885d84feb2d9dc14
adomb <adom.bence97@gmail.com>
2020-12-11 12:50:57 +0100

    create project

---
+++ InternetesLogin/app/src/main/java/hu/adombence/interneteslogin/User.java
@@ -0,0 +1,24 @@
+package hu.adombence.interneteslogin;
+
+public class User {
+    private String username, email;
+    private int id;
+
+    public User(int id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
