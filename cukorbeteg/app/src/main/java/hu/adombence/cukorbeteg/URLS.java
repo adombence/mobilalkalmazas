7868a274487ff02d0336903d2812c89f51ca0577
adombence <adom.bence97@gmail.com>
2020-12-16 19:31:22 +0100

    Update URLS.java

--- cukorbeteg/app/src/main/java/hu/adombence/cukorbeteg/URLS.java
+++ cukorbeteg/app/src/main/java/hu/adombence/cukorbeteg/URLS.java
@@ -2,6 +2,9 @@ package hu.adombence.cukorbeteg;
 
 public class URLS {
     private static final String URL_ROOT = "https://www.adombence.hu/serverside/api.php?action=";
+    private static final String URL_DATAS = "https://www.adombence.hu/serverside/datas.php?action=";
     public static final String URL_REGISTER = URL_ROOT + "signup";
     public static final String URL_LOGIN = URL_ROOT + "login";
+    public static final String URL_GET = URL_DATAS + "GET";
+    public static final String URL_POST = URL_DATAS + "POST";
 }
