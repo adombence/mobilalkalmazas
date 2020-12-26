519ad1d01a667a83696a145d3fd997209960d77e
adombence <adom.bence97@gmail.com>
2020-12-26 18:16:38 +0100

    Update datas.php

--- serverside/datas.php
+++ serverside/datas.php
@@ -8,7 +8,7 @@ $gender = mysqli_real_escape_string($conn, $_REQUEST['gender']);
 $age = mysqli_real_escape_string($conn, $_REQUEST['age']);
 $weight = mysqli_real_escape_string($conn, $_REQUEST['weight']);
 $height = mysqli_real_escape_string($conn, $_REQUEST['height']);
-echo "action=" . $action . "id=" . $id . "gender=" . $gender . "age=" . $age . "weight=" . $weight ;
+echo "action=" . $action . "id=" . $id . "gender=" . $gender . "age=" . $age . "weight=" . $weight .  "height=" . $height;
 /*echo "\taction: " . $action . "\n";
 echo "\tid: " . $id . "\n";
 echo "\tgender: " . $gender . "\n";
@@ -16,12 +16,12 @@ echo "\tage: " . $age . "\n";
 echo "\tweight: " . $weight . "\n";
 echo "\theight: " . $height . "\n";*/
 
-    $SQL = "INSERT INTO `datas`(`id`, `gender`, `age`, `weight`) VALUES (" . $id . ',' . $gender . ',' . $age . ',' . $weight . ');';
+$SQL = "INSERT INTO `datas`(`id`, `gender`, `age`, `weight`, `height`) VALUES  (" . $id . ',' . $gender . ',' . $age . ',' . $weight . ',' . $height . ');';
 
-    if (mysqli_query($conn, $SQL)) {
-        echo "Records added successfully.";
-    } else {
-        echo "ERROR: Could not able to execute $SQL. " . mysqli_error($conn);
-    }
+if (mysqli_query($conn, $SQL)) {
+    echo "Records added successfully.";
+} else {
+    echo "ERROR: Could not able to execute $SQL. " . mysqli_error($conn);
+}
 /*echo json_encode($response);*/
-?>
+
