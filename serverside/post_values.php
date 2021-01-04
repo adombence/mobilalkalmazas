4339d4756e5af1c577618cb4733e15495e82b6fd
adombence <adom.bence97@gmail.com>
2021-01-04 19:52:12 +0100

    Update post_values.php

--- serverside/post_values.php
+++ serverside/post_values.php
@@ -1,1 +1,18 @@
 <?php
+require_once 'db.php';
+
+$id = mysqli_real_escape_string($conn, $_REQUEST['id']);
+$value = mysqli_real_escape_string($conn, $_REQUEST['value']);
+$etkezesElott = mysqli_real_escape_string($conn, $_REQUEST['etkezeselott']);
+$energy = mysqli_real_escape_string($conn, $_REQUEST['energy']);
+$carbonhydrate = mysqli_real_escape_string($conn, $_REQUEST['$carbonhydrate']);
+
+
+$SQL = "INSERT INTO `sugar`(`id`, `value`, `etkezesElott`, `energy`, `carbohydrate`) VALUES (" . $id . ',' . $value . ',' . $etkezesElott . ',' . $energy . ',' . $carbonhydrate . ');';
+
+if (mysqli_query($conn, $SQL)) {
+    echo "Records added successfully.";
+} else {
+    echo "ERROR: Could not able to execute $SQL. " . mysqli_error($conn);
+}
+?>
