7b08295702903076787faea9f70172381275cb32
adombence <adom.bence97@gmail.com>
2021-01-04 18:18:02 +0100

    Update get_values.php

--- serverside/get_values.php
+++ serverside/get_values.php
@@ -5,7 +5,7 @@ $id = mysqli_real_escape_string($conn, $_REQUEST['id']);
 
 $response = array();
 
-$sql = "SELECT * FROM `sugar` WHERE id= " . $id;
+$sql = "SELECT * FROM `sugar` WHERE id = " . $id . " ORDER BY `sugar`.`date` DESC";
 $result = $conn->query($sql) or die($conn->error);
 
 while ($rows = mysqli_fetch_assoc($result)) {
