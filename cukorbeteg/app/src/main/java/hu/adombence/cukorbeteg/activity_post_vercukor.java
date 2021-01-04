38e236a07066438dff8f562bd9a3a4c9ea041d5a
adombence <adom.bence97@gmail.com>
2021-01-04 18:17:26 +0100

    Create activity_post_vercukor.java

---
+++ cukorbeteg/app/src/main/java/hu/adombence/cukorbeteg/activity_post_vercukor.java
@@ -0,0 +1,14 @@
+package hu.adombence.cukorbeteg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_post_vercukor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_vercukor);
+    }
+}
